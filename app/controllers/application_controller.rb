b62c4db1f6e0871227017fdb61ba719c4d3ef7e7
dddrvshhh <dddrvsh11@yahoo.co.jp>
2020-07-13 17:14:22 +0900

    5. ユーザー情報編集機能の追加 6. ログアウト機能の追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
+  before_action :authenticate_user!  
+  #ログインユーザーによってのみ実行可能
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
