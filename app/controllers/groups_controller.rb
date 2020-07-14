e050276dc4b6bb74eabccd62b47f95c4550a129f
dddrvshhh <dddrvsh11@yahoo.co.jp>
2020-07-14 14:56:44 +0900

    ビューのデザイン パスの指定 部分テンプレート form_with トップページの表示を追加

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,18 +1,35 @@
 class GroupsController < ApplicationController
 
+  def index
+  end
+
   def new
-    @group = Group.new   #インスタンス(データ、レコード)を生成
-    @group.users << current_user   #配列(データ)に要素(current_user)を加えます
+    @group = Group.new   #モデルメソッド.new インスタンス(データ、レコード)を生成
+    @group.users << current_user   #配列(データテーブル).users?に要素(current_user)を加えます
   end
 
   def create
     @group = Group.new(group_params)
-    if @group.save
+    if @group.save        #モデルメソッド.save インスタンス(データ、レコード)を保存
       redirect_to root_path, notice: 'グループを作成しました'
     else
       render :new
     end
   end
+
+  def edit 
+    @group = Group.find(params[:id])       #モデルメソッド.find(テーブルid)
+  end
+
+  def update
+    @group = Group.find(params[:id])     #モデルメソッド.find(テーブルid)
+    if @group.update(group_params)
+      redirect_to root_path, notice: 'グループを更新しました'
+    else
+      render :edit
+    end
+  end
+
 
   private      #新たに定義するコントローラーアクション
   def group_params
