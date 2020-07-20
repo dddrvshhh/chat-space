2176d9833865c84468910e7da140a633713bf492
dddrvshhh <dddrvsh11@yahoo.co.jp>
2020-07-20 21:55:59 +0900

    API作成4567

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,2 +1,20 @@
 $(function(){
-});
+  $('.chat_main__message-form3__longbox4').on('submit', function(e){
+    e.preventDefault();
+    console.log("hoge")
+
+    let formData = new FormData(this);
+    let url = $(this).attr('action')
+
+    $.ajax({
+      url: url,  //同期通信でいう『パス』
+      type: 'POST',  //同期通信でいう『HTTPメソッド』
+      data: formData,  
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+
+  })
+})
+
