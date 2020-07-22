a2f967982f43ed207ea8cf845193132b05550907
dddrvshhh <dddrvsh11@yahoo.co.jp>
2020-07-22 11:26:31 +0900

    自動更新1234

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -5,7 +5,7 @@ $(function(){
     if ( message.image ) {
       let html =
       //メッセージに画像が含まれる場合のHTMLを作る
-        `<div class="chat_main__message-list__messagebox">
+        `<div class="chat_main__message-list__messagebox" data-message-id=${message.id}>
           <div class="chat_main__message-list__messagebox__info-ss">
             <div class="chat_main__message-list__messagebox__info-ss__name-ww">
               ${message.user_name}
@@ -26,7 +26,7 @@ $(function(){
     } else {
       let html =
       //メッセージに画像が含まれない場合のHTMLを作る
-      `<div class="chat_main__message-list__messagebox">
+      `<div class="chat_main__message-list__messagebox" data-message-id=${message.id}>
         <div class="chat_main__message-list__messagebox__info-ss">
           <div class="chat_main__message-list__messagebox__info-ss__name-ww">
             ${message.user_name}
@@ -70,4 +70,37 @@ $(function(){
       $('.chat_main__message-form3__longbox4__sendtcontent5').prop('disabled', false);
     })
   });
+
+  let reloadMessages = function() {
+    //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
+    let last_message_id = $('.MessageBox:last').data("message-id") || 0;
+    $.ajax({
+      //ルーティングで設定した通り/groups/id番号/api/messagesとなるよう文字列を書く
+      url: "api/messages",
+      //ルーティングで設定した通りhttpメソッドをgetに指定
+      type: 'get',
+      dataType: 'json',
+      //dataオプションでリクエストに値を含める
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      // 更新するメッセージがなかった場合は.doneの後の処理が動かないようにする
+      if (messages.length !== 0) {
+        //追加するHTMLの入れ物を作る
+        let insertHTML = '';
+        //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
+        $.each(messages, function(i, message) {
+          insertHTML += buildHTML(message)
+        });
+        //メッセージが入ったHTMLに、入れ物ごと追加
+        $('.MessageField').append(insertHTML);
+      }
+    })
+    .fail(function() {
+      alert('error');
+    });
+  };
+
+
+
 });
