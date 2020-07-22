643ef93b16465baa6713300e8d50f93eb90a7527
dddrvshhh <dddrvsh11@yahoo.co.jp>
2020-07-22 12:13:14 +0900

    5 autoのクラス名修正後

--- app/assets/javascripts/autoReload.js
+++ app/assets/javascripts/autoReload.js
@@ -1,10 +1,7 @@
 $(function(){
-
   function buildHTML(message){
-    // 「もしメッセージに画像が含まれていたら」という条件式
     if ( message.image ) {
       let html =
-      //メッセージに画像が含まれる場合のHTMLを作る
         `<div class="chat_main__message-list__messagebox" data-message-id=${message.id}>
           <div class="chat_main__message-list__messagebox__info-ss">
             <div class="chat_main__message-list__messagebox__info-ss__name-ww">
@@ -22,11 +19,9 @@ $(function(){
           </div>
         </div>`
       return html;
-
     } else {
       let html =
-      //メッセージに画像が含まれない場合のHTMLを作る
-      `<div class="chat_main__message-list__messagebox" data-message-id=${message.id}>
+      `<div class="class="chat_main__message-list__messagebox" data-message-id=${message.id}>
         <div class="chat_main__message-list__messagebox__info-ss">
           <div class="chat_main__message-list__messagebox__info-ss__name-ww">
             ${message.user_name}
@@ -44,32 +39,6 @@ $(function(){
       return html;
     };
   }
-
-  $('.chat_main__message-form3__longbox4').on('submit', function(e){
-    e.preventDefault();
-    let formData = new FormData(this);
-    let url = $(this).attr('action')
-    $.ajax({
-      url: url,  //同期通信でいう『パス』
-      type: "POST",  //同期通信でいう『HTTPメソッド』
-      data: formData,
-      dataType: 'json',
-      processData: false,
-      contentType: false
-    })
-    .done(function(data){     // 非同期通信に成功した時の記述
-      let html = buildHTML(data);
-      $('.chat_main__message-list').append(html);      
-      $('.chat_main__message-form3__longbox4')[0].reset();
-      $('.chat_main__message-list').animate({ scrollTop: $('.chat_main__message-list')[0].scrollHeight});
-    })
-    .fail(function() {
-      alert("メッセージ送信に失敗しました");
-    })
-    .always(function() {
-      $('.chat_main__message-form3__longbox4__sendtcontent5').prop('disabled', false);
-    })
-  });
 
   let reloadMessages = function() {
     //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
@@ -93,14 +62,13 @@ $(function(){
           insertHTML += buildHTML(message)
         });
         //メッセージが入ったHTMLに、入れ物ごと追加
-        $('.MessageField').append(insertHTML);
+        $('.chat_main__message-list').append(insertHTML);
+        $('.chat_main__message-list').animate({ scrollTop: $('.chat_main__message-list')[0].scrollHeight});
       }
     })
     .fail(function() {
       alert('error');
     });
   };
-
-
-
+  setInterval(reloadMessages, 7000);
 });
